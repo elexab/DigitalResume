8028cd70d3af17bd9efae297adcac30f6df48f1a
Elexa Evans <elexaevans@gmail.com>
2019-09-08 21:37:37 -0500

    homepage complete, add pages

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("me").innerHTML = `<a id="about" href="about.html">About Me</a>`;
+document.getElementById("portfolio").innerHTML = `<a id="portfolio" href="portfolio.html">Portfolio</a>`;
+
+document.addEventListener("click", function(e){
+    if(e.target && e.target.id=="about"){
+        
+    }
+})
